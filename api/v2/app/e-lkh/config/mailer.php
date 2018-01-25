8a49bcd081aaed3ea4a110e5be267271d408ff99
londomloto <roso.sasongko@gmail.com>
2018-01-25 20:24:53 +0700

    .

--- api/v2/app/e-lkh/config/mailer.php
+++ api/v2/app/e-lkh/config/mailer.php
@@ -7,8 +7,8 @@ return array(
     // 'smtp_host' => 'mail.smartfren.com',
     // 'smtp_port' => 25,
 
-    'smtp_host' => 'mail.kct.co.id',
-    'smtp_port' => 587,
+    'smtp_host' => 'mail.pusdikadm.xyz',
+    'smtp_port' => 25,
 
     'smtp_user' => 'roso@kct.co.id',
     'smtp_pass' => 'roso123',
