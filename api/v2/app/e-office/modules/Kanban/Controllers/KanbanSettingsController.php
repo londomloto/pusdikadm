eaf8f48cecd36591d9615dfe479a871c2a3c7ecc
londomloto <roso.sasongko@gmail.com>
2018-02-12 06:09:33 +0700

    .

--- api/v2/app/e-office/modules/Kanban/Controllers/KanbanSettingsController.php
+++ api/v2/app/e-office/modules/Kanban/Controllers/KanbanSettingsController.php
@@ -13,7 +13,7 @@ class KanbanSettingsController extends \Micro\Controller {
 
     public function findByIdAction($id) {
         $result = KanbanSetting::get($id);
-        print_r($result);
+        print_r($result->toArray());
         
         $request = $this->request->getQuery('request');
         $project = $this->request->getQuery('project');
