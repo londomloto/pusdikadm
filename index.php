fde755847d5e9447488ae84a4582f21ec84ef0ba
londomloto <roso.sasongko@gmail.com>
2018-03-04 22:58:24 +0700

    .

---
+++ index.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, minimum-scale=1, initial-scale=1, user-scalable=yes">
+    <title>pusdikadm.xyz</title>
+    <style>
+        html,body {
+            padding: 0;
+            margin: 0;
+            width: 100%;
+            height: 100%;
+        }
+        body {
+            font-family: 'Segoe UI', 'Roboto', 'Arial', sans-serif;
+            overflow: hidden;
+        }
+        body:before {
+            content: '';
+            position: absolute;
+            top: 50%;
+            border-top: 1px solid #dfdfdf;
+            left: 0;
+            right: 0;
+        }
+        h3 {
+            font-size: 20px;
+            font-weight: 300;
+        }
+        .container {
+            text-align: center;
+            position: absolute;
+            left: 50%;
+            top: 50%;
+            margin-left: -105px;
+            margin-top: -50px;
+        }
+        .item {
+            display: inline-block;
+            width: 100px;
+            height: 100px;
+            line-height: 100px;
+            text-align: center;
+            background-color: #dfdfdf;
+            border-radius: 50%;
+            font-size: 16px;
+            font-weight: 300;
+            /*text-transform: uppercase;*/
+            text-decoration: none;
+            color: #666;
+            -webkit-transition: background-color 0.2s ease-in;
+            transition: background-color 0.2s ease-in;
+            float: left;
+            margin: 0 5px 0 0;
+        }
+        .item + .item {
+            margin: 0 0 0 5px;
+        }
+        .item:hover {
+            background-color: #00C853;
+            color: #fff;
+        }
+    </style>
+</head>
+<body>
+
+    <div class="container">
+        <div class="menu">
+            <a class="item" href="e-lkh"><b>e</b>-lkh</a>
+            <a class="item" href="e-office"><b>e</b>-office</a>
+        </div>
+    </div>
+
+</body>
+</html>
