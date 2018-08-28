1aa58c5251de4a84eb848d1129b4c33051f52b7a
londomloto <roso.sasongko@gmail.com>
2018-08-28 21:38:22 +0700

    update mailer

--- api/v2/app/e-zip/config/mailer.php
+++ api/v2/app/e-zip/config/mailer.php
@@ -5,10 +5,17 @@ return array(
     'type' => 'html',
 
     'accounts' => array(
-        'no-reply' => array('support@pusdikadm.xyz' => 'Admin Pusdiklat'),
-        'admin' => array('support@pusdikadm.xyz' => 'Admin Pusdiklat'),
-        'support' => array('support@pusdikadm.xyz', 'Admin Pusdiklat')
+        'no-reply' => array('pusdikadm.xyz@gmail.com' => 'Admin Pusdiklat'),
+        'admin' => array('pusdikadm.xyz@gmail.com' => 'Admin Pusdiklat'),
+        'support' => array('pusdikadm.xyz@gmail.com', 'Admin Pusdiklat')
     ),
+
+    'smtp_host' => 'smtp.gmail.com',
+    'smtp_port' => 465,
+    'smtp_auth' => true,
+    'smtp_user' => 'pusdikadm.xyz',
+    'smtp_pass' => 'pu5d1k4dm123',
+    'smtp_secure' => 'tls'
 
     // 'smtp_host' => 'mail.pusdikadm.xyz',
     // 'smtp_port' => 465,
@@ -17,10 +24,10 @@ return array(
     // 'smtp_pass' => 'pu5d1k4dm123',
     // 'smtp_secure' => 'none'
     
-    'smtp_host' => '192.168.0.100',
-    'smtp_port' => 587,
-    'smtp_auth' => true,
-    'smtp_user' => 'support@192.168.0.100',
-    'smtp_pass' => '123',
-    'smtp_secure' => 'none'
+    // 'smtp_host' => '192.168.0.100',
+    // 'smtp_port' => 587,
+    // 'smtp_auth' => true,
+    // 'smtp_user' => 'support@192.168.0.100',
+    // 'smtp_pass' => '123',
+    // 'smtp_secure' => 'none'
 );
